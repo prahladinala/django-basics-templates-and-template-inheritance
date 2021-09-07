7b753fc2f1cca349bc4c32ccad09ba1d1c260cfa
Prahlad Inala <prahlad2018@gmail.com>
2021-09-07 18:55:24 +0530

    organizing templates file structure as django recommends

--- projects/views.py
+++ projects/views.py
@@ -4,8 +4,10 @@ from django.http import HttpResponse
 
 def projects(request):
     # return HttpResponse('Here are our projects')
-    return render(request, 'projects.html')
+    # return render(request, 'projects.html')
+    return render(request, 'projects/projects.html')
 
 def project(request, pk):
     # return HttpResponse('Single Project' + ' ' + str(pk))
-    return render(request, 'single-project.html')
+    # return render(request, 'single-project.html')
+    return render(request, 'projects/single-project.html')
