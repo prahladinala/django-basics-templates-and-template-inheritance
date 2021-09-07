1124149c748371297d13cc9483870f27fd4373c9
Prahlad Inala <prahlad2018@gmail.com>
2021-09-07 18:31:54 +0530

    Render HTML Templates to path

--- projects/views.py
+++ projects/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def projects(request):
-    return HttpResponse('Here are our projects')
+    # return HttpResponse('Here are our projects')
+    return render(request, 'projects.html')
 
 def project(request, pk):
-    return HttpResponse('Single Project' + ' ' + str(pk))
+    # return HttpResponse('Single Project' + ' ' + str(pk))
+    return render(request, 'single-project.html')
